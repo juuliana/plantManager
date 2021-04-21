ce68a44cf353d6c885be3547243cec09460762e2
juuliana <jumaju10@hotmail.com>
2021-04-21 18:25:44 -0300

    add UserIdentification --

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 
 export default function App(){
   return(
-    <Welcome/>
+    <UserIdentification/>
   )
 }
