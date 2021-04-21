1df9818e50de086578915d1e023c558c990e8ba2
juuliana <jumaju10@hotmail.com>
2021-04-21 18:28:00 -0300

    Welcome --add

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,78 +1,95 @@
-import React from 'react';
-import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
-import wateringImg from '../assets/watering.png';
-import colors from '../styles/colors';
+import React from "react";
 
-export function Welcome(){
-    return(
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
+import {
+  Text,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from "react-native";
 
-            <Image
-                source={wateringImg}
-                style={styles.image}
-                resizeMode="contain"
-            />
+import { Feather } from "@expo/vector-icons";
 
-            <Text  style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+import wateringImg from "../assets/watering.png";
 
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.7}
-            >
-                <Text style={styles.buttonText}>
-                    --
-                </Text>
-            </TouchableOpacity>
-            
-        </SafeAreaView>
-    )
+import colors from "../styles/colors";
+
+export function Welcome() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de
+          {"\n"}
+          forma fácil
+        </Text>
+
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around'
-    },
+  container: {
+    flex: 1,
+  },
+  
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
+  },
 
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: colors.heading,
-        marginTop: 38
-    },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+    lineHeight: 34,
+  },
 
-    subtitle: {
-        textAlign: 'center',
-        fontSize: 18,
-        paddingHorizontal: 20,
-        color: colors.heading
-    },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
 
-    image: {
-        height: Dimensions.get('window').width * 0.7
-    },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
+  },
 
-    button: {
-        backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        width: 56
-    },
-    
-    buttonText: {
-        color: colors.white,
-        fontSize: 24
-    }
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
+  },
 });
